e0a702a055c2ea16bbbaa72e042d83a446762013
David Hranicky <djh30000@gmail.com>
2022-03-31 12:52:57 -0400

    Fixed path to corpus on linux

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         try (Scanner input = new Scanner(System.in)) {
-            File inFile = new File("corpi\\beemovie.txt");
+            File inFile = new File("corpi/beemovie.txt");
             TokenProvider provider = new TokenProvider(inFile);
             Levenshtein l = new Levenshtein();
             String searchWord;
