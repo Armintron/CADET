aeb0f0836a574c5d6e506e9a8a3226369cd7fe0e
Armin <nimra7701@gmail.com>
2022-04-04 13:19:26 -0400

    restore phonetics on algrunner

--- src/AlgRunner.java
+++ src/AlgRunner.java
@@ -46,6 +46,7 @@ public class AlgRunner implements Runnable {
     public ConcurrentSkipListSet<WordScoreEntry> scoreMap;
     StringDistance alg;
     TokenProvider provider;
+    Phonetic phonetic;
     String searchWord;
 
     /**
@@ -55,15 +56,33 @@ public class AlgRunner implements Runnable {
      * @param searchWord Word to search for
      */
     public AlgRunner(StringDistance msd, TokenProvider provider, String searchWord) {
+        this(msd, provider, searchWord, null);
+    }
+
+    public AlgRunner(StringDistance msd, TokenProvider provider, String searchWord, Phonetic phonetic)
+    {
+        Levenshtein l = new Levenshtein();
         this.scoreMap = new ConcurrentSkipListSet<>();
         this.searchWord = searchWord;
         this.provider = provider;
         this.alg = msd;
-
+        this.phonetic = phonetic;
     }
 
     @Override
     public void run() {
+        // cache phonetic representation of the search word if we have an encoder
+        String searchPhonetic = "";
+        if(phonetic != null) searchPhonetic = phonetic.ToPhonetic(searchWord);
+        // our phonetic algorithm isnt useful for this word
+        if(searchPhonetic == null) 
+        {
+            System.out.println("The search word " + searchWord
+                    + " has no meaningful phonetic representation with the current encoder: "
+                    + phonetic.encoder.toString());
+            return;
+        }
+        
         while (provider.hasNextWord()) {
             String cur = null;
             while (cur == null) {
@@ -73,7 +92,18 @@ public class AlgRunner implements Runnable {
                 }
                 cur = provider.getWord();
             }
-            Double score = alg.distance(searchWord, cur);
+            Double score;
+            // encode words and then find score
+            if(phonetic != null)
+            {
+                String curPhonetic = phonetic.ToPhonetic(cur);
+                // skip over words that dont have a phonetic representation
+                if (curPhonetic == null)
+                    continue;
+                score = alg.distance(searchPhonetic, curPhonetic);
+            }
+            // find score for words without encoding
+            else score = alg.distance(searchWord, cur);
             scoreMap.add(new WordScoreEntry(cur, score));
 
         }
